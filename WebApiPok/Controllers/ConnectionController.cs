e8267e9b91708bc263b3e16bfdf58fb54c492250
Jean Luc BIAH <jeanlucbiah.dev@outlook.com.com>
2018-10-22 16:41:09 +0200

    Ajoutez les fichiers .gitignore et .gitattributes.

---
+++ WebApiPok/Controllers/ConnectionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiPok.Controllers
+{
+    [Route("conn/")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        /// <summary>
+        /// https://localhost:44342/conn/Authenticate?email=user&password=test
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Authenticate")]
+        public ActionResult<bool> Authenticate(string email, string password)
+        {
+            return (email == "user" && password == "test");
+        }
+    }
+}
